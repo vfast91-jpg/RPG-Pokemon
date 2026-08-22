152f0d25ba759b122099d065d780fb3be7b3acdf
vfast91-jpg <vfast91@googlemail.com>
2026-08-22 07:58:57 +0200

    Update Raupy regression count for Taubsi runtime

--- tests/caterpie_family_tm_mechanics_test.gd
+++ tests/caterpie_family_tm_mechanics_test.gd
@@ -26,7 +26,7 @@ func _initialize() -> void:
 
 func _assert_inventory(lab) -> void:
     var moves: Dictionary = lab.data.get("moves", {})
-    assert(moves.size() == 228, "Runtime muss nach dem Bibor-Paket 228 Attacken enthalten.")
+    assert(moves.size() == 229, "Runtime muss mit dem Taubsi-Paket 229 Attacken enthalten.")
     for move_id: String in NEW_MOVE_IDS + CORRECTED_MOVE_IDS:
         assert(moves.has(move_id), "Raupy-Familienattacke fehlt: " + move_id)
         var runtime: Dictionary = (moves[move_id] as Dictionary).get("runtime", {})
